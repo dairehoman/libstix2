734ac351d718edc4830756520e907226716f001c
Daire Homan <dairehoman@gmail.com>
2018-04-24 14:28:33 +0100

    added marking defs

---
+++ objects/markingDefinition.go
@@ -0,0 +1,28 @@
+package objects
+
+import (
+	"github.com/freetaxii/libstix2/objects/properties"
+)
+
+//MarkingDefintionType -
+type MarkingDefintionType struct {
+	properties.CommonMarkingDefinitionPropertiesType
+	DefinitionType string `json:"definition_type"`
+	Definition     struct {
+		Tlp string `json:"tlp"`
+	} `json:"definition"`
+}
+
+//NewMarkingDefinition -
+func NewMarkingDefinition(ver string, colour string) *MarkingDefintionType {
+	var obj MarkingDefintionType
+	obj.InitMarkingDefinitionObjectProperties("marking_definition", ver)
+	obj.addType("tlp")
+	obj.Definition.Tlp = colour
+	return &obj
+}
+
+func (o *MarkingDefintionType) addType(s string) error {
+	o.DefinitionType = s
+	return nil
+}
